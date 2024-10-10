2d6e77be71775ee7d4f083a3a9ad608df48efdbd
justanotheranonymoususer <justanotheranonymoususer@users.noreply.github.com>
2024-10-10 12:21:01 +0300

    format etc

--- test.py
+++ test.py
@@ -10,6 +10,6 @@ with open("memtest.efi.mui", "rb") as f:
         print(signed_data.signer_info.program_name)
         if signed_data.signer_info.countersigner is not None:
             print(signed_data.signer_info.countersigner.signing_time)
-            times += [signed_data.signer_info.countersigner.signing_time]
+            times += [str(signed_data.signer_info.countersigner.signing_time)]
 
-assert times == ["2016-08-06T11:59:52.180000+00:00"]
+assert times == ["2016-08-06 11:59:52.180000+00:00"]
